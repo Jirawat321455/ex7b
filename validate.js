8a9221ad76eed324fc5b1b2b108fc5878297b8a0
Jirawat <2410511101025@live4.utcc.ac.th>
2026-03-04 15:04:17 +0700

    Bug fix#2 – adding student id validation

--- validate.js
+++ validate.js
@@ -1,34 +1,40 @@
 function checkSID() {
-  let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function checkCandiNo() {
-  let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
+	let sid = (document.getElementById("sid").value).trim();
+  
+	// ต้องยาว 10 ตัว และเป็นตัวเลข 0-9 เท่านั้น
+	let sidPattern = /^[0-9]{10}$/;
+  
+	if (sidPattern.test(sid)) {
+	  return true;
+	} else {
 	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
-			alert("Your input data passes validation!!");
-			return true;
-		}
 	}
-}
+  }
+  
+  function checkCandiNo() {
+	let candi = (document.getElementById("candi").value).trim();
+  
+	// ต้องเป็นตัวเลข และมากกว่าหรือเท่ากับ 1
+	if (isNaN(candi) || candi < 1) {
+	  return false;
+	} else {
+	  return true;
+	}
+  }
+  
+  function validateForm(){
+	  if(!checkSID()){
+		alert("Invalid value for Student ID! (Must be 10 digits)");
+		document.getElementById("sid").focus();
+		return false;
+	  }else{
+		  if(!checkCandiNo()){
+			alert("Invalid value for Candidate No!!");
+			document.getElementById("candi").focus();
+			return false;
+		  }else{
+			  alert("Your input data passes validation!!");
+			  return true;
+		  }
+	  }
+  }
