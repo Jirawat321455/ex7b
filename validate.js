9bdeaca25cea1ea3bc3c563ff9a30d0b3ea0f7ca
Jirawat <2410511101025@live4.utcc.ac.th>
2026-03-04 15:06:16 +0700

    Bug fix#3 – revise candidate no. validation

--- validate.js
+++ validate.js
@@ -1,40 +1,42 @@
 function checkSID() {
 	let sid = (document.getElementById("sid").value).trim();
-  
+
 	// ต้องยาว 10 ตัว และเป็นตัวเลข 0-9 เท่านั้น
 	let sidPattern = /^[0-9]{10}$/;
-  
+
 	if (sidPattern.test(sid)) {
-	  return true;
+		return true;
 	} else {
-	  return false;
+		return false;
 	}
-  }
-  
-  function checkCandiNo() {
+}
+
+function checkCandiNo() {
 	let candi = (document.getElementById("candi").value).trim();
-  
-	// ต้องเป็นตัวเลข และมากกว่าหรือเท่ากับ 1
-	if (isNaN(candi) || candi < 1) {
-	  return false;
+
+	// ต้องเป็นตัวเลขจำนวนเต็ม 1 ถึง 10 เท่านั้น
+	let candiPattern = /^(10|[1-9])$/;
+
+	if (candiPattern.test(candi)) {
+		return true;
 	} else {
-	  return true;
+		return false;
 	}
-  }
-  
-  function validateForm(){
-	  if(!checkSID()){
+}
+
+function validateForm(){
+	if(!checkSID()){
 		alert("Invalid value for Student ID! (Must be 10 digits)");
 		document.getElementById("sid").focus();
 		return false;
-	  }else{
-		  if(!checkCandiNo()){
-			alert("Invalid value for Candidate No!!");
+	}else{
+		if(!checkCandiNo()){
+			alert("Invalid value for Candidate No!! (Must be integer 1-10)");
 			document.getElementById("candi").focus();
 			return false;
-		  }else{
-			  alert("Your input data passes validation!!");
-			  return true;
-		  }
-	  }
-  }
+		}else{
+			alert("Your input data passes validation!!");
+			return true;
+		}
+	}
+}
